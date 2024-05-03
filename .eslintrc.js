2556b500aae4f5585a6660bbd98e64209fdfac8d
Kyle Scudder <kyle@kylescudder.co.uk>
2024-05-03 17:21:08 +0100

    feat(lint): updated linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,58 +1,52 @@
-module.export = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: ['standard-with-typescript', 'plugin:react/recommended'],
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    project: true
   },
-  plugins: ['react'],
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  },
+  plugins: ['@typescript-eslint', 'drizzle'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked'
+  ],
   rules: {
-    '@typescript-eslint/semi': 'off',
-    curly: 'warn',
-    eqeqeq: 'warn',
-    'no-throw-literal': 'warn',
-    semi: ['error', 'never'],
-    'linebreak-style': [
-      'error',
-      process.platform === 'win32' ? 'windows' : 'unix'
-    ],
-    indent: ['error', 'tab'],
-    '@typescript-eslint/indent': ['error', 'tab'],
-    'no-tabs': 'off',
-    quotes: ['error', 'single'],
-    'import/extensions': 'off',
-    'no-new': 'off',
-    'no-underscore-dangle': ['error', { allow: ['_id'] }],
-    'comma-dangle': ['error', 'never'],
-    'max-len': ['error', { code: 110 }],
-    '@typescript-eslint/naming-convention': [
-      'error',
+    '@typescript-eslint/array-type': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
       {
-        selector: 'variable',
-        format: ['camelCase', 'PascalCase']
-      },
-      {
-        selector: 'typeProperty',
-        format: ['camelCase'],
-        leadingUnderscore: 'allow',
-        trailingUnderscore: 'allow'
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports'
       }
     ],
-    'no-await-in-loop': 'off',
-    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_'
+      }
+    ],
+    '@typescript-eslint/require-await': 'off',
     '@typescript-eslint/no-misused-promises': [
       'error',
       {
-        checksVoidReturn: false
+        checksVoidReturn: {
+          attributes: false
+        }
+      }
+    ],
+    'drizzle/enforce-delete-with-where': [
+      'error',
+      {
+        drizzleObjectName: ['db']
+      }
+    ],
+    'drizzle/enforce-update-with-where': [
+      'error',
+      {
+        drizzleObjectName: ['db']
       }
     ]
   }
 }
+module.exports = config
