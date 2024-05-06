9dc644539827c2d41b3cd63438e407acf6c18f8d
scudderkct <74150974+kylescudder@users.noreply.github.com>
2024-05-06 10:40:26 +0100

    chore(husky): added husk to project

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,22 +7,20 @@ import { currentUser } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
 import { List } from '@/components/shared/List'
 
-async function Page (): Promise<JSX.Element> {
-	const user = await currentUser()
-	if (!user) return <></>
+async function Page(): Promise<JSX.Element> {
+  const user = await currentUser()
+  if (!user) return <></>
 
-	const userInfo = await getUserInfo(user.id)
-	if (!userInfo?.onboarded) redirect('/onboarding')
+  const userInfo = await getUserInfo(user.id)
+  if (!userInfo?.onboarded) redirect('/onboarding')
 
-	const recipes = await getRecipes()
+  const recipes = await getRecipes()
 
-	return (
-		<div>
-			<List
-				records={recipes}
-			/>
-		</div>
-	)
+  return (
+    <div>
+      <List records={recipes} />
+    </div>
+  )
 }
 
 export default Page
--- components/shared/Logout.tsx
+++ components/shared/Logout.tsx
@@ -5,21 +5,25 @@ import { SignOutButton, SignedIn } from '@clerk/nextjs'
 import { IconLogout } from '@tabler/icons-react'
 import { useRouter } from 'next/navigation'
 
-export default function Logout (): JSX.Element {
-	const router = useRouter()
-	return (
-		<SignedIn>
-			<SignOutButton signOutCallback={() => { router.push('/sign-in') }}>
-				<div className="flex items-center">
-					<IconLogout
-						size={24}
-						className="text-light-1"
-						stroke={1}
-						strokeLinejoin="miter"
-					/>
-					<p className="text-light-2 max-lg:hidden ml-2">Logout</p>
-				</div>
-			</SignOutButton>
-		</SignedIn>
-	)
+export default function Logout(): JSX.Element {
+  const router = useRouter()
+  return (
+    <SignedIn>
+      <SignOutButton
+        signOutCallback={() => {
+          router.push('/sign-in')
+        }}
+      >
+        <div className='flex items-center'>
+          <IconLogout
+            size={24}
+            className='text-light-1'
+            stroke={1}
+            strokeLinejoin='miter'
+          />
+          <p className='text-light-2 max-lg:hidden ml-2'>Logout</p>
+        </div>
+      </SignOutButton>
+    </SignedIn>
+  )
 }
